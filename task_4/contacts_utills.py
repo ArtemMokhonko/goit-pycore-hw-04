35fb68c830f039cc19e77c30fabc6e18b8949f00
ArtemMokhonko <artem.stayl@gmail.com>
2024-04-27 11:49:16 +0300

    task_4 added

---
+++ task_4/contacts_utills.py
@@ -0,0 +1,33 @@
+def add_contact(args: list[str], contacts: dict):
+    try:
+        name, phone = args
+        contacts[name] = phone
+        return "Contact added."
+    except ValueError:
+        return f"not enough values to unpack (expected 2, got: {len(args)})"
+    
+def change_contact(args: list[str], contacts: dict):
+    try:
+        name, phone = args
+        contacts[name] = phone    
+
+    except ValueError:
+        return f"not enough values to unpack (expected 2, got: {len(args)})"
+
+
+def show_phone(args: list[str], contacts: dict):
+    try:
+        name, _ = args
+        print(name)
+        return contacts[name]
+    except KeyError:
+        return f"Contact doesn't exist: {name})"
+
+
+def all_contacts(contacts: dict):
+    str_s = 'All contacts'
+    for name, phone in contacts.items():
+        str_s += f"{name} {phone} \n"
+    
+    return str_s[:len(str_s)-1]
+    
